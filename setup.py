4333aec959c60d5c7f633eb5adba2cc0dbf2b7d3
AliSot2000 <alisot200@gmail.com>
2023-08-12 17:01:04 +0200

    + First version of Packaging stuff.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  # packages = ['fast_diff_py'],   # Chose the same as "name"
+  version = '0.1.0',      # Start with a small number and increase it with every change you make
+  url = 'https://github.com/AliSot2000/quick_n_dirty_mp',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/AliSot2000/quick_n_dirty_mp/archive/refs/tags/v0.1.0.tar.gz',    # I explain this later on
+  keywords = ['python', 'multiprocessing', 'asynchronous', 'ProcessPoolExecutor', 'ThreadPoolExecutor',
+              'Machine Learning', "producer-consumer", "embedded"],   # Keywords that define your package best
+)
